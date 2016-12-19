0b80f14d15e5bad05186ba4a94b2428b9d8d30af
amazingfly <derekstich@gmail.com>
2016-12-19 01:56:06 +0000

    systemd fixed

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo systemctl restart pilight
-/home/pi/outlets/outlets
+#./home/pi/outlets/outlets
 #sudo chgrp -R staff /usr/local/go
 #export GOROOT=/usr/local/go
 #export PATH="$PATH:$GOROOT/bin"
